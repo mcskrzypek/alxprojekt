5c20c7e9b55419a3251837e6282e3ace5f0d1e8b
Lukasz R <lukaszrudzki@outlook.com>
2017-05-16 22:31:27 +0200

    polecenia sql

--- dane.sql
+++ dane.sql
@@ -1,26 +1,16 @@
-Tabela:
-uzytkownicy
-    id_uzytkownik primary key
-    id_uprawnienia klucz obcy (uprawnienia_uzytkownika)
-    login
-    imie
-    nazwisko
-    haslo
-    notatki text
-    aktywny (bool)
+CREATE TABLE uzytkownicy (
+    id_uzytkownik serial primary key,
+    id_uprawnienia integer NOT NULL references uprawnienia_uzytkownika,
+    login varchar(100) NOT NULL,
+    imie varchar(100) NOT NULL,
+    nazwisko varchar(100) NOT NULL,
+    haslo varchar(250) NOT NULL,
+    notatki text,
+    aktywny boolean DEFAULT true NOT NULL
+);
+        
+CREATE TABLE uprawnienia_uzytkownika (
+    id_uprawnienia serial primary key,
+    rola varchar(15) NOT NULL
+);
     
-    
-Tabela:
-historia_uzytkownikow
-    id_historia primary key
-    id_zmieniacz
-    id_zmieniany
-    akcja
-    data timestamp default now()
-    adr_ip
-    
-    
-Tabela:
-uprawnienia_uzytkownika
-    id_uprawnienia primary key
-    rola    (np pracownik, kierownik, )
