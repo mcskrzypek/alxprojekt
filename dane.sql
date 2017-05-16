addb802105c3bd1d05efa69ef5eb3a676a740aa2
mcskrzypek <michal.skrzypek@lucart.pl>
2017-05-16 22:41:33 +0200

    prawie nic

--- dane.sql
+++ dane.sql
@@ -9,22 +9,20 @@ CREATE TABLE uzytkownicy (
     notatki text,
     aktywny boolean DEFAULT true NOT NULL
 );
-        
+
 CREATE TABLE uprawnienia_uzytkownika (
     id_uprawnienia serial primary key,
     rola varchar(15) NOT NULL
 );
-    
-
-    CREATE TABLE historia_uzytkownikow (
-        id_historia serial primary key,
-        data timestamp without time zone DEFAULT now() NOT NULL,
-        id_zmieniacz integer NOT NULL references uzytkownicy,
-        adr_ip varchar(15),
-        id_zmieniany integer NOT NULL references uzytkownicy,
-        akcja int(1),
-        tresc text NOT NULL,
-        wpis_id integer NOT NULL references wpisy
-    );
 
 
+CREATE TABLE historia_uzytkownikow (
+    id_historia serial primary key,
+    data timestamp without time zone DEFAULT now() NOT NULL,
+    id_zmieniacz integer NOT NULL references uzytkownicy,
+    adr_ip varchar(15),
+    id_zmieniany integer NOT NULL references uzytkownicy,
+    akcja int(1),
+    tresc text NOT NULL,
+    wpis_id integer NOT NULL references wpisy
+);
