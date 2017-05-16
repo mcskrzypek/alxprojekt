51ca41bea500862d21f263de706426811e5f354c
mcskrzypek <michal.skrzypek@lucart.pl>
2017-05-16 22:30:44 +0200

    tabele

--- dane.sql
+++ dane.sql
@@ -8,18 +8,26 @@ uzytkownicy
     haslo
     notatki text
     aktywny (bool)
-    
-    
+
+    CREATE TABLE historia_uzytkownikow (
+        id_historia serial primary key,
+        data timestamp without time zone DEFAULT now() NOT NULL,
+        id_zmieniacz integer NOT NULL references uzytkownicy,
+        adr_ip varchar(15),
+        id_zmieniany integer NOT NULL references uzytkownicy,
+        akcja int(1),
+        tresc text NOT NULL,
+        wpis_id integer NOT NULL references wpisy
+    );
+
+
+
 Tabela:
 historia_uzytkownikow
-    id_historia primary key
-    id_zmieniacz
-    id_zmieniany
     akcja
-    data timestamp default now()
     adr_ip
-    
-    
+
+
 Tabela:
 uprawnienia_uzytkownika
     id_uprawnienia primary key
